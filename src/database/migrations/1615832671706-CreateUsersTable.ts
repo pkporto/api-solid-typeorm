cb891192cf69e8a38123511bdf3752e3e5348860
Patrick Porto <patrickporto.dev@gmail.com>
2021-03-15 16:54:46 -0300

    ultimas migrations

--- src/database/migrations/1615832671706-CreateUsersTable.ts
+++ src/database/migrations/1615832671706-CreateUsersTable.ts
@@ -1,8 +1,23 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsersTable1615832671706 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        new Table({
+            name: 'stuff',
+            columns: [
+              {
+                name: 'id',
+                type: 'uuid',
+                isPrimary: true
+              },
+              {
+                name: 'userId',
+                type: 'uuid'
+              }
+            ]
+          })
+        ;
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
